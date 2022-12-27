fa8f115f9016829f8391d858c0a76930243090f6
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-12-26 23:45:28 -0300

    temporal home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { Inter } from "@next/font/google";
 import { ShopLayout } from "../components/layouts";
+import { initialData } from "../database/products";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +23,21 @@ export default function Home() {
       <Typography variant="h2" component="h2" sx={{ mb: 1 }}>
         Todos los productos
       </Typography>
+      <Grid container spacing={4}>
+        {initialData.products.map((product) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={product.slug}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={`products/${product.images[0]}`}
+                  alt={product.title}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </ShopLayout>
   );
 }
