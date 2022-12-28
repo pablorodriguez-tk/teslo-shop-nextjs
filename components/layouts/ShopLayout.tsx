64e2a989ceffa5100f76ff2aec1c532af1194128
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-12-28 20:48:51 -0300

    side menu

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { FC, PropsWithChildren } from "react";
-import { Navbar } from "../ui";
+import { Navbar, SideMenu } from "../ui";
 
 interface Props extends PropsWithChildren {
   title: string;
@@ -26,7 +26,7 @@ export const ShopLayout: FC<Props> = ({
       <nav>
         <Navbar />
       </nav>
-      {/* TODO: Sidebar */}
+      <SideMenu />
       <main
         style={{
           margin: "80px auto",
