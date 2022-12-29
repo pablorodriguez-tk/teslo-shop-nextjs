42f10a286cd5c59abfada8694879add376c07a05
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-12-28 21:10:51 -0300

    card and listcards component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
-import {
-  Card,
-  CardActionArea,
-  CardMedia,
-  Grid,
-  Typography,
-} from "@mui/material";
+import { Typography } from "@mui/material";
 import { Inter } from "@next/font/google";
 import { ShopLayout } from "../components/layouts";
+import { ProductList } from "../components/products";
 import { initialData } from "../database/products";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,21 +18,7 @@ export default function Home() {
       <Typography variant="h2" component="h2" sx={{ mb: 1 }}>
         Todos los productos
       </Typography>
-      <Grid container spacing={4}>
-        {initialData.products.map((product) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={product.slug}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  image={`products/${product.images[0]}`}
-                  alt={product.title}
-                />
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+      <ProductList products={initialData.products as any} />
     </ShopLayout>
   );
 }
