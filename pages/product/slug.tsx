3bff6a5c908e7bae0c7700e348496ff06537c2ef
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-12-29 23:50:28 -0300

    slideshow and counter on product page

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,6 +1,8 @@
 import { ShopLayout } from "../../components/layouts/ShopLayout";
 import { initialData } from "../../database/products";
 import { Box, Button, Chip, Grid, Typography } from "@mui/material";
+import { ProductSlideshow } from "../../components/products";
+import { ItemCounter } from "../../components/ui";
 
 const product = initialData.products[0];
 
@@ -9,7 +11,7 @@ const ProductPage = () => {
     <ShopLayout title={product.title} pageDescription={product.description}>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={7}>
-          {/* slideshow */}
+          <ProductSlideshow images={product.images} />
         </Grid>
         <Grid item xs={12} sm={5}>
           <Box display="flex" flexDirection="column">
@@ -23,7 +25,7 @@ const ProductPage = () => {
             {/* cantidad */}
             <Box sx={{ my: 2 }}>
               <Typography variant="subtitle2">Cantidad</Typography>
-              {/* itemcounter */}
+              <ItemCounter />
             </Box>
             {/* agregar al carrito */}
             <Button
