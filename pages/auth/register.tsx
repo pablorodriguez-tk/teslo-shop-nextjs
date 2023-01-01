a27d13ac1783a2b22e7837bce61b42d2b9f8a010
Pablo Rodriguez <pablorodriguez88@gmail.com>
2023-01-01 19:50:38 -0300

    login and register page

---
+++ pages/auth/register.tsx
@@ -0,0 +1,53 @@
+import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../../components/layouts";
+import NextLink from "next/link";
+
+const RegisterPage = () => {
+  return (
+    <AuthLayout title="Register">
+      <Box sx={{ width: 350, padding: "10px 20px" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h1" component="h1">
+              Crear cuenta
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="Nombre completo" variant="filled" fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="Correo" variant="filled" fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              variant="filled"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              color="secondary"
+              className="circular-btn"
+              size="large"
+              fullWidth
+              variant="contained"
+            >
+              Ingresar
+            </Button>
+          </Grid>
+
+          <Grid item xs={12}>
+            <NextLink href="/auth/login" passHref legacyBehavior>
+              <Link style={{ textDecoration: "underline" }}>
+                ¿Ya tienes cuenta?
+              </Link>
+            </NextLink>
+          </Grid>
+        </Grid>
+      </Box>
+    </AuthLayout>
+  );
+};
+export default RegisterPage;
