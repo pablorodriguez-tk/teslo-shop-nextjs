3a6e928cf3ab60d3f52d93c9ed71ba3f26111b33
Pablo Rodriguez <pablorodriguez88@gmail.com>
2023-01-06 00:11:17 -0300

    cart context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
 import { lightTheme } from "../themes";
 import { SWRConfig } from "swr";
-import { UiProvider } from "../context";
+import { CartProvider, UiProvider } from "../context";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,11 +13,13 @@ export default function App({ Component, pageProps }: AppProps) {
           fetch(resource, init).then((res) => res.json()),
       }}
     >
-      <UiProvider>
-        <ThemeProvider theme={lightTheme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </UiProvider>
+      <CartProvider>
+        <UiProvider>
+          <ThemeProvider theme={lightTheme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </UiProvider>
+      </CartProvider>
     </SWRConfig>
   );
 }
