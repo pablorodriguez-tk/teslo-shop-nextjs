44a03985dc8c9d931b742b457dad5c5f2b125482
Pablo Rodriguez <pablorodriguez88@gmail.com>
2023-01-07 14:25:43 -0300

    user register api

---
+++ pages/api/user/register.ts
@@ -0,0 +1,97 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../database";
+import { User } from "../../../models";
+import bcrypt from "bcryptjs";
+import { jwt } from "../../../utils";
+
+type Data =
+  | {
+      message: string;
+    }
+  | {
+      token: string;
+      user: {
+        email: string;
+        name: string;
+        role: string;
+      };
+    };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "POST":
+      return registerUser(req, res);
+
+    default:
+      res.status(400).json({ message: "Bad request" });
+  }
+}
+
+const registerUser = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  const { email, password, name } = req.body as {
+    email: string;
+    password: string;
+    name: string;
+  };
+
+  if (!email || !password || !name) {
+    return res.status(400).json({
+      message: "Debe de especificar el nombre, el email y el password",
+    });
+  }
+
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "La contraseña debe tener al menos 6 caracteres" });
+  }
+
+  if (name.length < 2) {
+    return res
+      .status(400)
+      .json({ message: "El nombre debe tener al menos 2 caracteres" });
+  }
+
+  //TODO:validar email
+
+  await db.connect();
+  const user = await User.findOne({ email }).lean();
+
+  if (user) {
+    await db.disconnect();
+    return res.status(400).json({ message: "Ese email ya está registrado" });
+  }
+
+  const newUser = new User({
+    email: email.toLocaleLowerCase(),
+    password: bcrypt.hashSync(password),
+    role: "client",
+    name,
+  });
+
+  try {
+    await newUser.save({ validateBeforeSave: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error al crear el usuario" });
+  }
+
+  const { _id, role } = newUser;
+
+  const token = jwt.signToken(_id, email);
+
+  res.status(200).json({
+    token,
+    user: {
+      email,
+      name,
+      role,
+    },
+  });
+};
