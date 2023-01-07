752cd44877f1886a7b0e8e4ab07325d7335d2b71
Pablo Rodriguez <pablorodriguez88@gmail.com>
2023-01-07 15:25:55 -0300

    validate token

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -8,3 +8,19 @@ export const signToken = (_id: string, email: string) => {
     expiresIn: "30d",
   });
 };
+
+export const isValidToken = async (token: string): Promise<string> => {
+  if (!process.env.JWT_SECRET_SEED)
+    throw new Error("JWT_SECRET_SEED is not defined");
+  return new Promise((resolve, reject) => {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET_SEED || "", (err, payload) => {
+        if (err) return reject("JWT no es válido");
+        const { _id } = payload as { _id: string };
+        resolve(_id);
+      });
+    } catch (error) {
+      reject("JWT no es válido");
+    }
+  });
+};
