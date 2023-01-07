eb59218b6590776b0b9d27b9e6b6cdfa5d602cca
Pablo Rodriguez <pablorodriguez88@gmail.com>
2023-01-07 14:10:42 -0300

    json web token

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../../../database";
 import { User } from "../../../models";
 import bcrypt from "bcryptjs";
+import { jwt } from "../../../utils";
 
 type Data =
   | {
@@ -49,10 +50,12 @@ const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     return res.status(400).json({ message: "Email o contraseña incorrectos" });
   }
 
-  const { role, name } = user;
+  const { role, name, _id } = user;
+
+  const token = jwt.signToken(_id, email);
 
   res.status(200).json({
-    token: "",
+    token,
     user: {
       email,
       name,
